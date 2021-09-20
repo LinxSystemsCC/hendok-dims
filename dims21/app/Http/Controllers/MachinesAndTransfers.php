ea60e2ad1127fc152a303f1f7a08371dfeba4a1f
reginald_ledwaba <ledwaba.reginald@gmail.com>
2021-09-20 16:07:35 +0200

    extra files

---
+++ dims21/app/Http/Controllers/MachinesAndTransfers.php
@@ -0,0 +1,14 @@
+<?php
+
+
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use \Cache;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\SalesForm;
+class MachinesAndTransfers extends Controller
+{
+    
+}
