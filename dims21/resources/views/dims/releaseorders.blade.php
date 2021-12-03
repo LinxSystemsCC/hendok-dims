8205b1b08f39882e2eaf0c3d79ae35cb5139f34b
reginald_ledwaba <ledwaba.reginald@gmail.com>
2021-12-03 13:47:43 +0200

    release

--- dims21/resources/views/dims/releaseorders.blade.php
+++ dims21/resources/views/dims/releaseorders.blade.php
@@ -273,7 +273,8 @@ OrderID selected <input type="text" id="account">
                 $.each(data, function (key, value) {
                     if(value.CustomerOnHold !="0"){
                          classes = 'backgroudcolorbad';
-                    }else{
+                    }
+                    if(value.CustomerOnHold =="2"){
                          classes = 'backgroudnormal';
                     }
                     trHTML += '<tr role="row" class="fast_removeOrders '+classes+'"  style="font-size: 13px;color:black"><td>' +
