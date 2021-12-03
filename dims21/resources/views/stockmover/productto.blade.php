eac06ca81ce2d725b875f70b857c60ba2d7243b6
reginald_ledwaba <ledwaba.reginald@gmail.com>
2021-12-03 13:22:36 +0200

    getextracomunsforItems

--- dims21/resources/views/stockmover/productto.blade.php
+++ dims21/resources/views/stockmover/productto.blade.php
@@ -25,17 +25,23 @@
 <body>
 
 <div>
-    Shelf From:{{$shelffrom}} - Shelf To: {{$shelfto}}
+    Shelf From:{{$shelffrom}} <br>Shelf To: {{$shelfto}}
 
-    <label>Scan Product {{$productfrom}}</label><br>
-    <input type="text" id="productto" value="" required style="width: 100%;height:95px;">
-    <input type="text" id="confirmqty" value="" required style="width: 100%;height:60px;">
+    <form action="{{url('gofinish')}}" method="post">
+        @csrf
+    <label style="font-size: 21px;font-weight: 900">Scan Product {{$productfrom}}</label><br>
+    <input type="text" id="productto" value="" required style="width: 100%;height:95px;"><br>
+        <label style="font-size: 21px;font-weight: 900">Quantity</label><br>
+    <input type="text" id="confirmqty" value="" required style="width: 100%;height:60px;"><br>
+        <label style="font-size: 21px;font-weight: 900">Expiry Date</label><br>
+        <input type="date" id="expiry" value="" required style="width: 100%;height:60px;"><br>
     <input type="hidden" id="shelffrom"  value="{{$shelffrom}}">
     <input type="hidden" id="productfrom"  value="{{$productfrom}}">
     <input type="hidden" id="shelfto"  value="{{$shelfto}}">
     <input type="hidden" id="Qty"  value="{{$Qty}}">
     <br><br><br>
-    <button id="nextfromshelf" style="background: green;font-weight: 900;color:black;width: 100%;height:65px;color: white;font-weight: 900">NEXT</button>
+        <input type="submit" value="NEXT" style="background: #802e6a;font-weight: 900;color:black;width: 100%;height:65px;color: white;font-weight: 900">
+    </form>
 </div>
 
 <script type="text/javascript" charset="utf-8">
