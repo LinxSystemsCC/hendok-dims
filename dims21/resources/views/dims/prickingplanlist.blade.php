4534419e485ea22c75847c9443e4c6333407f0bd
reginald_ledwaba <ledwaba.reginald@gmail.com>
2022-03-04 13:15:38 +0200

    subtotal

--- dims21/resources/views/dims/prickingplanlist.blade.php
+++ dims21/resources/views/dims/prickingplanlist.blade.php
@@ -146,7 +146,7 @@
                     <td></td>
                     <td></td>
                     <td></td>
-                    <td></td>
+                    <td> {{$subtotal}}</td>
 
                     <td></td>
                 </tr>
