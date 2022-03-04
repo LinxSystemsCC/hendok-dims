d6482d104f41e47247aa7bc085f437bc3867d682
reginald_ledwaba <ledwaba.reginald@gmail.com>
2022-03-04 15:05:30 +0200

    moved to live

--- dims21/resources/views/dims/prickingplanlist.blade.php
+++ dims21/resources/views/dims/prickingplanlist.blade.php
@@ -47,109 +47,110 @@
     </style>
 </head>
 <body style="font-family: Sans-serif">
-    <div class="container">
+<div class="container">
 
-        <table>
-            <tbody>
-            <tr>
-                <td  class="col-xs-2">Date Loaded</td>
-                <td  class="col-xs-2" style="width: 117px;"> </td>
-                <td  class="col-xs-2">Dunnages #</td>
-                <td  class="col-xs-2" style="width: 117px;"></td>
-                <td  class="col-xs-2">Load Completed</td>
-                <td  class="col-xs-2" style="width: 117px;"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Time Loaded</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Straps #</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2">Load Secured</td>
-                <td  class="col-xs-2"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Team Leader</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Pallets #</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2">Team Leader Signed</td>
-                <td  class="col-xs-2"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Checker Name</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Plastic Corners #</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Ticket #</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Tarps #</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2">Stands #</td>
-                <td  class="col-xs-2"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Driver Name</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Trailer Type</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2">Falcon Sign</td>
-                <td  class="col-xs-2"></td>
-            </tr>
-            <tr>
-                <td  class="col-xs-2">Horse Reg#</td>
-                <td  class="col-xs-2"> </td>
-                <td  class="col-xs-2">Trailer Reg #</td>
-                <td  class="col-xs-2"></td>
-                <td  class="col-xs-2">Trailer No.</td>
-                <td  class="col-xs-2"></td>
-            </tr>
+    <table>
+        <tbody>
+        <tr>
+            <td  class="col-xs-2">Date Loaded</td>
+            <td  class="col-xs-2" style="width: 117px;"> </td>
+            <td  class="col-xs-2">Dunnages #</td>
+            <td  class="col-xs-2" style="width: 117px;"></td>
+            <td  class="col-xs-2">Load Completed</td>
+            <td  class="col-xs-2" style="width: 117px;"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Time Loaded</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Straps #</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2">Load Secured</td>
+            <td  class="col-xs-2"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Team Leader</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Pallets #</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2">Team Leader Signed</td>
+            <td  class="col-xs-2"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Checker Name</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Plastic Corners #</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Ticket #</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Tarps #</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2">Stands #</td>
+            <td  class="col-xs-2"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Driver Name</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Trailer Type</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2">Falcon Sign</td>
+            <td  class="col-xs-2"></td>
+        </tr>
+        <tr>
+            <td  class="col-xs-2">Horse Reg#</td>
+            <td  class="col-xs-2"> </td>
+            <td  class="col-xs-2">Trailer Reg #</td>
+            <td  class="col-xs-2"></td>
+            <td  class="col-xs-2">Trailer No.</td>
+            <td  class="col-xs-2"></td>
+        </tr>
 
-            </tbody>
-        </table>
-<hr><br>
-        <table id="orderHeaderPrint" style="font-size: 11px;">
-            <thead>
-            <tr style="    background: black;    color: white;" >
-                <th class="col-xs-2">Storename</th>
-                <th class="col-xs-1">Area</th>
-                <th class="col-xs-2" style="width: 80px">Order Date</th>
-                <th class="col-xs-2">Sales Order No</th>
-                <th class="col-xs-2">Instruction</th>
-                <th class="col-xs-2">Line No</th>
-                <th class="col-xs-2">Description</th>
-                <th class="col-xs-2" style="width: 80px;" >Quantity</th>
-                <th class="col-xs-2" >Weights</th>
-
-
-                <th class="col-xs-2" >To Invoice</th>
+        </tbody>
+    </table>
+    <hr><br>
+    <table id="orderHeaderPrint" style="font-size: 11px;">
+        <thead>
+        <tr style="    background: black;    color: white;" >
+            <th class="col-xs-2">Storename</th>
+            <th class="col-xs-1">Area</th>
+            <th class="col-xs-2" style="width: 80px">Order Date</th>
+            <th class="col-xs-2">Sales Order No</th>
+            <th class="col-xs-2">Instruction</th>
+            <th class="col-xs-2">Line No</th>
+            <th class="col-xs-2">Description</th>
+            <th class="col-xs-2" style="width: 80px;" >Quantity</th>
+            <th class="col-xs-2" >Weights</th>
 
 
-            </tr>
-            </thead>
-            <tbody >
-            <?php $storenames = "";$orderNumber=""; $subtotal=0;$Grandtotal=0;$area = "";$orderdate=""; $istrue = true;$count = 0; ?>
-            @foreach($listproducts as $val )
-            <?php $subtotal = $subtotal + floatval($val->weightPlanned);    ?>
-                <?php $Grandtotal = $Grandtotal + floatval($val->weightPlanned);?>
+            <th class="col-xs-2" >To Invoice</th>
+
+
+        </tr>
+        </thead>
+        <tbody >
+        <?php $storenames = "";$orderNumber=""; $subtotal=0;$Grandtotal=0;$area = "";$orderdate=""; $istrue = true;$count = 0; ?>
+        @foreach($listproducts as $val )
+            <?php      ?>
+            <?php $Grandtotal = $Grandtotal + floatval($val->weightPlanned);?>
             @if($storenames != $val->StoreName )
-                @if($count > 0 )
-                <tr style="background: darkgray;color: white; font-weight: 900;">
-                    <td></td>
-                    <td> </td>
-                    <td>NEXT</td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td></td>
-                    <td> </td>
 
-                    <td></td>
-                </tr>
+                @if($count > 0 )
+                    <tr style="background: darkgray;color: white; font-weight: 900;">
+                        <td></td>
+                        <td> </td>
+                        <td>NEXT</td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td>{{$subtotal}} </td>
+
+                        <td></td>
+                    </tr>
                     <?php $subtotal = 0;$count = 0; ?>
                 @endif
                 <tr>
@@ -171,9 +172,9 @@
                     <td></td>
 
 
-                </tr><?php $istrue = true; $storenames = $val->StoreName; $orderNumber =  $val->OrderNum;$area =  $val->areas;$orderdate = $val->OrderDate  ?>
+                </tr><?php $istrue = true; $storenames = $val->StoreName;$subtotal = $subtotal + floatval($val->weightPlanned);  $orderNumber =  $val->OrderNum;$area =  $val->areas;$orderdate = $val->OrderDate  ?>
 
-                @else
+            @else
                 <tr>
 
                     @if($storenames != $val->StoreName )
@@ -185,23 +186,23 @@
                         </td>
                     @endif
                     @if($area !=  $val->areas)
-                    <td>{{ $val->areas}}</td>
-                        @else
+                        <td>{{ $val->areas}}</td>
+                    @else
                         <td></td>
-                        @endif
-                        @if($orderdate != $val->OrderDate)
-                    <td>{{ $val->OrderDate}}</td>
-                        @else
-                            <td></td>
-                            @endif
+                    @endif
+                    @if($orderdate != $val->OrderDate)
+                        <td>{{ $val->OrderDate}}</td>
+                    @else
+                        <td></td>
+                    @endif
 
-                        @if($orderNumber != $val->OrderNum)
-                    <td>{{$val->OrderNum}}</td>
-                        @else
-                            <td style="">
+                    @if($orderNumber != $val->OrderNum)
+                        <td>{{$val->OrderNum}}</td>
+                    @else
+                        <td style="">
 
-                            </td>
-                            @endif
+                        </td>
+                    @endif
                     <td>{{ $val->ExtOrderNum}}</td>
                     <td>{{ $val->iLineID}}</td>
                     <td>{{ $val->PastelDescription}}</td>
@@ -213,31 +214,31 @@
 
 
                 </tr>
-                <?php $storenames = $val->StoreName; $orderNumber =  $val->OrderNum;$area =  $val->areas;
+                <?php $storenames = $val->StoreName;$subtotal = $subtotal + floatval($val->weightPlanned);  $orderNumber =  $val->OrderNum;$area =  $val->areas;
                 if($storenames == $val->StoreName){
                     $istrue = true;
                 }
                 ?>
-                @endif
+            @endif
             <?php $count++; ?>
-            @endforeach
-            <tr style="background: grey;color: white;font-weight: 900">
-                <td> Grand Total</td>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td> </td>
-                <td>{{ $Grandtotal}}</td>
+        @endforeach
+        <tr style="background: grey;color: white;font-weight: 900">
+            <td> Grand Total</td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td> </td>
+            <td>{{ $Grandtotal}}</td>
 
-                <td> </td>
-            </tr>
+            <td> </td>
+        </tr>
 
-            </tbody>
-        </table>
-    </div>
+        </tbody>
+    </table>
+</div>
 
 </body>
 <script src="{{ asset('js/jquery-2.2.3.min.js') }}"></script>
