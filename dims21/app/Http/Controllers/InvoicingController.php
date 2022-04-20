989d3efc8209d2c4d227b854e4fa018bd4665876
reginald_ledwaba <ledwaba.reginald@gmail.com>
2022-04-20 16:03:52 +0200

    test

--- dims21/app/Http/Controllers/InvoicingController.php
+++ dims21/app/Http/Controllers/InvoicingController.php
@@ -44,8 +44,9 @@ class InvoicingController extends Controller
                 ->select('exec spGetOrderNumbersLinesToProcess ?,?,?',
                     array($reference,$val->SalesOrderNo,$val->intOwnerID)
                 );
-            $x = $sdkHelper->GetSalesOrder($val->SalesOrderNo);
             var_dump($returnGetsalesorderNoLines);
+            $x = $sdkHelper->GetSalesOrder($val->SalesOrderNo);
+
             foreach ($returnGetsalesorderNoLines as $innverVal){
                 $lineno = $innverVal->LineNos - 1;
                 $x->Detail[$lineno]->ToProcess =floatval($innverVal->Toinvoice);
