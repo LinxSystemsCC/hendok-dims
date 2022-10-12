33cbd8825f8528e037949df30c0277fcee934f92
reginald_ledwaba <ledwaba.reginald@gmail.com>
2022-10-12 15:07:03 +0200

    menur

---
+++ dims21/resources/views/warehouse/menu.blade.php
@@ -0,0 +1,12 @@
+<a href='{!!url("/createPalletConfig")!!}'>Pallets Configurations</a>
+<a href='{!!url("/mapitemstopallet")!!}' >Map Pallet To Items</a>
+<a href='{!!url("/departmentpage")!!}'>Departments</a>
+<a href='{!!url("/machines")!!}'>Machines</a>
+<a href='{!!url("/mapmachinestodept")!!}' >Map Machines To Dept</a>
+<a href='{!!url("/mapitemsmachinesdept")!!}'>Map Machines,Dept & Prod</a>
+<a href='{!!url("/createjobs")!!}'   >Create Jobs</a>
+<a href='{!!url("/printpalletsselectdept")!!}' >Print Pallet Labels</a>
+<a href='{!!url("/location")!!}'  class="active">Locations</a>
+<a href='{!!url("/stocklocation")!!}' >Stock</a>
+<a href="#">In Progress Jobs</a>
+<a href="#">Jobs Data</a>
