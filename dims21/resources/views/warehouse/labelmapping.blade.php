006484c478bcd2bf667668ddead42747472df2d6
KyleWestran <kyle@lsystems.co.za>
2023-03-23 15:51:31 +0200

    added MORE!!!!

--- dims21/resources/views/warehouse/labelmapping.blade.php
+++ dims21/resources/views/warehouse/labelmapping.blade.php
@@ -186,7 +186,7 @@
                     <select  class="form-select input-sm col-xs-1" id="labelname" required>
                         <option></option>
                         @foreach($label as $val)
-                            <option value="{{$val->intLabelTypeID}}">{{$val->strLabelName}} - {{$val->strLabelConfig}}</option>
+                            <option value="{{$val->intLabelTypeID}}">{{$val->strLabelName}} - {{$val->strLabelConfig}} - {{$val->intLabelTypeID}}</option>
                         @endforeach
                     </select>
                 </div>               
