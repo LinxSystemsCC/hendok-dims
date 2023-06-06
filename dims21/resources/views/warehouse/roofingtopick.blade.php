fd588f001e89eca3ef3a1a9c0244803de18e8ba1
Raesetja-reffs <reginald@lsystems.co.za>
2023-06-06 13:26:24 +0200

    dsds

--- dims21/resources/views/warehouse/roofingtopick.blade.php
+++ dims21/resources/views/warehouse/roofingtopick.blade.php
@@ -1,1 +1,121 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- CSS only -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.13.2/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+    <script src="https://code.jquery.com/ui/1.13.2/jquery-ui.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.5/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/7.4.0/polyfill.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/exceljs/4.1.1/exceljs.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/FileSaver.js/2.0.2/FileSaver.min.js"></script>
+    <link href="{{ asset('css/jquery.flexdatalist.min.css') }}" rel="stylesheet"  type='text/css'>
+    <link rel="stylesheet" href="https://cdn3.devexpress.com/jslib/20.1.7/css/dx.common.css">
+    <link rel="stylesheet" href="https://cdn3.devexpress.com/jslib/20.1.7/css/dx.light.css">
+
+
+    <script src="{{ asset('js/jquery-ui.js') }}"></script>
+    <script src="{{ asset('js/jquery.dialogextend.js') }}"></script>
+    <script src="{{ asset('js/jquery.flexdatalist.min.js') }}"></script>
+    <!-- DevExtreme library -->
+    <script type="text/javascript" src="https://cdn3.devexpress.com/jslib/20.1.7/js/dx.all.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+
+
+    <style>
+        .vertical-menu {
+            width: 200px;
+        }
+
+        .vertical-menu a {
+            background-color: #eee;
+            color: black;
+            display: block;
+            padding: 12px;
+            text-decoration: none;
+        }
+
+        .vertical-menu a:hover {
+            background-color: #ccc;
+        }
+
+        .vertical-menu a.active {
+            background-color: #04AA6D;
+            color: white;
+        }
+    </style>
+
+
+</head>
+<div class="col-lg-12"  style="padding:20px; min-height: 100vh; min-width: 100%;">
+
+
+    <h2>Choose Customer</h2>
+    @foreach($sonumberandunickref as $val)
+
+                <button class="btn btn-danger" type="button" style="width:100% !important;font-size: 20px;" onclick="location.href='{!!url("/startgenratingqrcodeforpallet")!!}/{{$val->intJobId}}/None'" >
+                    {{$val->PastelDescription}} {{$val->productionstat}} [ {{$val->strPalletTypeDescription}} ]</button>
+                <br>
+                <br>
+    @endforeach
+
+</div>
+
+
+<style>
+
+    .dx-datagrid-table{
+        font-size:15px;
+    }
+</style>
+
+<script>
+    $.ajaxSetup({
+        headers: {
+            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+        }
+    });
+    $( document ).on( 'focus', ':input', function(){
+        $( this ).attr( 'autocomplete', 'off' );
+    });
+
+
+    function showDialog(tag,width,height)
+    {
+        $( tag ).dialog({height: height, modal: false,
+            width: width,containment: false}).dialogExtend({
+            "closable" : true, // enable/disable close button
+            "maximizable" : false, // enable/disable maximize button
+            "minimizable" : true, // enable/disable minimize button
+            "collapsable" : true, // enable/disable collapse button
+            "dblclick" : "collapse", // set action on double click. false, 'maximize', 'minimize', 'collapse'
+            "titlebar" : false, // false, 'none', 'transparent'
+            "minimizeLocation" : "right", // sets alignment of minimized dialogues
+            "icons" : { // jQuery UI icon class
+
+                "maximize" : "ui-icon-circle-plus",
+                "minimize" : "ui-icon-circle-minus",
+                "collapse" : "ui-icon-triangle-1-s",
+                "restore" : "ui-icon-bullet"
+            },
+            "load" : function(evt, dlg){ }, // event
+            "beforeCollapse" : function(evt, dlg){ }, // event
+            "beforeMaximize" : function(evt, dlg){ }, // event
+            "beforeMinimize" : function(evt, dlg){ }, // event
+            "beforeRestore" : function(evt, dlg){ }, // event
+            "collapse" : function(evt, dlg){  }, // event
+            "maximize" : function(evt, dlg){ }, // event
+            "minimize" : function(evt, dlg){  }, // event
+            "restore" : function(evt, dlg){  } // event
+        });
+    }
+
+</script>
