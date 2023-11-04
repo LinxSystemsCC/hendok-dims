7268554aec785bd896a7be68d43c131abb30b8a7
Raesetja-reffs <reginald@lsystems.co.za>
2023-11-04 22:22:41 +0200

    changed to use the DefaultRoute !=8

--- dims21/resources/views/warehouse/teamleaderpickloadtable.blade.php
+++ dims21/resources/views/warehouse/teamleaderpickloadtable.blade.php
@@ -1,33 +1,33 @@
 <table id="pickLoadTable" class="table mb-0">
     <thead class="sticky-top">
-        <tr class="bg-dark text-white fw-bold">
-            <th class="col-xs-2">Order Date</th>
-            <th class="col-xs-2">SO Number</th>
-            <th class="col-xs-2">Description</th>
-            <th class="col-xs-2">Weight</th>
-            <th class="col-xs-2">Qty</th>
-            <th class="col-xs-2">Picked</th>
-            <th class="col-xs-2">Loaded</th>
-            @if(isset($includePriority) && $includePriority)
-                <th class="col-xs-2">Priority</th>
-            @endif
-        </tr>
+    <tr class="bg-dark text-white fw-bold">
+        <th class="col-xs-2">Order Date</th>
+        <th class="col-xs-2">SO Number</th>
+        <th class="col-xs-2">Description</th>
+        <th class="col-xs-2">Weight</th>
+        <th class="col-xs-2">Qty</th>
+        <th class="col-xs-2">Picked</th>
+        <th class="col-xs-2">Loaded</th>
+        @if(isset($includePriority) && $includePriority)
+            <th class="col-xs-2">Priority</th>
+        @endif
+    </tr>
     </thead>
     <tbody>
-        <?php
-        $storenames = "";
-        $orderNumber = "";
-        $area = "";
-        $orderdate = "";
-        $istrue = true;
-        $count = 0;
-        $totalWeight = 0;
-        $totalQty = 0;
-        $totalPickedQty = 0;
-        $totalLoadedQty = 0;
-        ?>
+    <?php
+    $storenames = "";
+    $orderNumber = "";
+    $area = "";
+    $orderdate = "";
+    $istrue = true;
+    $count = 0;
+    $totalWeight = 0;
+    $totalQty = 0;
+    $totalPickedQty = 0;
+    $totalLoadedQty = 0;
+    ?>
 
-        @foreach($listproducts as $val)
+    @foreach($listproducts as $val)
 
         <?php
         $externalCount = 0;
@@ -38,140 +38,140 @@
         ?>
 
         @if($count == 0)
-        <tr class="bg-secondary text-white fw-bold" id="{{$ID}}">
-            <td colspan="8">STOP: {{$val->intSequence}} - {{ $val->StoreName}}</td>
-        </tr>
+            <tr class="bg-secondary text-white fw-bold" id="{{$ID}}">
+                <td colspan="8">STOP: {{$val->intSequence}} - {{ $val->StoreName}}</td>
+            </tr>
         @endif
         @if($storenames != $val->StoreName)
-        @if($count > 0)
-        <tr class="bg-dark">
-            <td colspan="3" class="text-white fw-bold">Totals</td>
-            <td class="text-white fw-bold">{{$totalWeight}}</td>
-            <td class="text-white fw-bold">{{$totalQty}}</td>
-            <td class="text-white fw-bold">{{$totalPickedQty}}</td>
-            <td class="text-white fw-bold">{{$totalLoadedQty}}</td>
-            @if(isset($includePriority) && $includePriority)
-                <td class="fw-bold">
-                    
-                </td>
+            @if($count > 0)
+                <tr class="bg-dark">
+                    <td colspan="3" class="text-white fw-bold">Totals</td>
+                    <td class="text-white fw-bold">{{$totalWeight}}</td>
+                    <td class="text-white fw-bold">{{$totalQty}}</td>
+                    <td class="text-white fw-bold">{{$totalPickedQty}}</td>
+                    <td class="text-white fw-bold">{{$totalLoadedQty}}</td>
+                    @if(isset($includePriority) && $includePriority)
+                        <td class="fw-bold">
+
+                        </td>
+                    @endif
+                </tr>
+                <tr class="bg-secondary text-white fw-bold">
+                    <td colspan="8">STOP: {{$val->intSequence}} - {{ $val->StoreName}}</td>
+                </tr>
             @endif
-        </tr>
-        <tr class="bg-secondary text-white fw-bold">
-            <td colspan="8">STOP: {{$val->intSequence}} - {{ $val->StoreName}}</td>
-        </tr>
-        @endif
-        <?php
-        // Reset totals for the new sequence
-        $totalWeight= 0;
-        $totalQty = 0;
-        $totalPickedQty = 0;
-        $totalLoadedQty = 0;
-        ?>
-        @if($val->isLineInvoiced == 1)
-        <tr id="rtrr{{$ID}}" @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
-        @else
-        <tr id="rtrr{{$ID}}" @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
-        @endif
-        <td>{{ $val->OrderDate}}</td>
-        <td>{{$val->OrderNum}}</td>
-            @if(Auth::user()->DefaultRoute == 8)
-        <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value={{ $val->hasLabel }}></td>
+            <?php
+            // Reset totals for the new sequence
+            $totalWeight= 0;
+            $totalQty = 0;
+            $totalPickedQty = 0;
+            $totalLoadedQty = 0;
+            ?>
+            @if($val->isLineInvoiced == 1)
+                <tr id="rtrr{{$ID}}" @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
             @else
-                <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value="0"></td>
-            @endif
-        <td>{{ floatval($val->weightPlanned) }}</td>
-        <td>{{ floatval($val->mnyQty)}}</td>
-        <td @if($val->mnyPickedQuantity < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
-            {{ floatval($val->mnyPickedQuantity)}}
-        </td>
-        <td @if($val->mnyLoadedQty < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
-            {{ floatval($val->mnyLoadedQty)}}
-            @if($val->sageWeight == 1)
-                <button class="btn btn-outline-dark btn-sm float-end btnFinishPickingWeighted" value={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i></button>
-            @endif
-        </td>
-        @if(isset($includePriority) && $includePriority)
-            <td class="fw-bold">
-                <button class="btn btn-sm btn-secondary w-100 btnSetPriority" value={{ $val->isPriorityLine}} name={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i>PRIORITY</button>
-            </td>
-        @endif
-        
-        </tr>
-        <?php
-        $istrue = true;
-        $storenames = $val->StoreName;
-        $orderNumber = $val->OrderNum;
-        $area = $val->areas;
-        $orderdate = $val->OrderDate;
-        $totalWeight += floatval($val->weightPlanned);
-        $totalQty += floatval($val->mnyQty);
-        $totalPickedQty += floatval($val->mnyPickedQuantity);
-        $totalLoadedQty += floatval($val->mnyLoadedQty);
-        ?>
-        @else
-        <tr @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
-            @if($orderdate != $val->OrderDate)
-            <td>{{ $val->OrderDate}}</td>
-            @else
-            <td></td>
-            @endif
-            @if($orderNumber != $val->OrderNum)
-            <td>{{$val->OrderNum}}</td>
-            @else
-            <td></td>
-            @endif
-                @if(Auth::user()->DefaultRoute == 8)
-                    <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value={{ $val->hasLabel }}></td>
+                <tr id="rtrr{{$ID}}" @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
+                    @endif
+                    <td>{{ $val->OrderDate}}</td>
+                    <td>{{$val->OrderNum}}</td>
+                    @if(Auth::user()->DefaultRoute != 8)
+                        <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value={{ $val->hasLabel }}></td>
+                    @else
+                        <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value="0"></td>
+                    @endif
+                    <td>{{ floatval($val->weightPlanned) }}</td>
+                    <td>{{ floatval($val->mnyQty)}}</td>
+                    <td @if($val->mnyPickedQuantity < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
+                        {{ floatval($val->mnyPickedQuantity)}}
+                    </td>
+                    <td @if($val->mnyLoadedQty < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
+                        {{ floatval($val->mnyLoadedQty)}}
+                        @if($val->sageWeight == 1)
+                            <button class="btn btn-outline-dark btn-sm float-end btnFinishPickingWeighted" value={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i></button>
+                        @endif
+                    </td>
+                    @if(isset($includePriority) && $includePriority)
+                        <td class="fw-bold">
+                            <button class="btn btn-sm btn-secondary w-100 btnSetPriority" value={{ $val->isPriorityLine}} name={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i>PRIORITY</button>
+                        </td>
+                    @endif
+
+                </tr>
+                <?php
+                $istrue = true;
+                $storenames = $val->StoreName;
+                $orderNumber = $val->OrderNum;
+                $area = $val->areas;
+                $orderdate = $val->OrderDate;
+                $totalWeight += floatval($val->weightPlanned);
+                $totalQty += floatval($val->mnyQty);
+                $totalPickedQty += floatval($val->mnyPickedQuantity);
+                $totalLoadedQty += floatval($val->mnyLoadedQty);
+                ?>
                 @else
-                    <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value="0"></td>
+                    <tr @if($val->isPriorityLine == '1') class='bg-danger text-white' @endif>
+                        @if($orderdate != $val->OrderDate)
+                            <td>{{ $val->OrderDate}}</td>
+                        @else
+                            <td></td>
+                        @endif
+                        @if($orderNumber != $val->OrderNum)
+                            <td>{{$val->OrderNum}}</td>
+                        @else
+                            <td></td>
+                        @endif
+                        @if(Auth::user()->DefaultRoute != 8)
+                            <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value={{ $val->hasLabel }}></td>
+                        @else
+                            <td>{{ $val->PastelDescription }}<input type="hidden" class="intAutoPickinghidden" value={{ $val->intAutoPicking }}>  <input type="hidden" class="hasLabel" value="0"></td>
+                        @endif
+                        <td>{{ floatval($val->weightPlanned) }}</td>
+                        <td>{{ floatval($val->mnyQty)}}</td>
+                        <td @if($val->mnyPickedQuantity < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
+                            {{ floatval($val->mnyPickedQuantity)}}
+                        </td>
+                        <td @if($val->mnyLoadedQty < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
+                            {{ floatval($val->mnyLoadedQty)}}
+                            @if($val->sageWeight == 1)
+                                <button class="btn btn-outline-dark btn-sm float-end btnFinishPickingWeighted" value={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i></button>
+                            @endif
+                        </td>
+                        @if(isset($includePriority) && $includePriority)
+                            <td class="fw-bold">
+                                <button class="btn btn-sm btn-secondary w-100 btnSetPriority" value={{ $val->isPriorityLine}} name={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i>PRIORITY</button>
+                            </td>
+                        @endif
+                    </tr>
+                    <?php
+                    $storenames = $val->StoreName;
+                    $orderNumber = $val->OrderNum;
+                    $orderdate = $val->OrderDate;
+                    $area = $val->areas;
+                    if ($storenames == $val->StoreName) {
+                        $istrue = true;
+                    }
+                    $totalWeight += floatval($val->weightPlanned);
+                    $totalQty += floatval($val->mnyQty);
+                    $totalPickedQty += floatval($val->mnyPickedQuantity);
+                    $totalLoadedQty += floatval($val->mnyLoadedQty);
+                    ?>
                 @endif
-            <td>{{ floatval($val->weightPlanned) }}</td>
-            <td>{{ floatval($val->mnyQty)}}</td>
-            <td @if($val->mnyPickedQuantity < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
-                {{ floatval($val->mnyPickedQuantity)}}
-            </td>
-            <td @if($val->mnyLoadedQty < $val->mnyQty) class='bg-warning  text-black' @else class='bg-success text-white' @endif>
-                {{ floatval($val->mnyLoadedQty)}}
-                @if($val->sageWeight == 1)
-                    <button class="btn btn-outline-dark btn-sm float-end btnFinishPickingWeighted" value={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i></button>
-                @endif
-            </td>
-            @if(isset($includePriority) && $includePriority)
-                <td class="fw-bold">
-                    <button class="btn btn-sm btn-secondary w-100 btnSetPriority" value={{ $val->isPriorityLine}} name={{ $val->intAutoPicking }}><i class="fa fa-check p-0"></i>PRIORITY</button>
-                </td>
-            @endif
-        </tr>
-        <?php
-        $storenames = $val->StoreName;
-        $orderNumber = $val->OrderNum;
-        $orderdate = $val->OrderDate;
-        $area = $val->areas;
-        if ($storenames == $val->StoreName) {
-            $istrue = true;
-        }
-        $totalWeight += floatval($val->weightPlanned);
-        $totalQty += floatval($val->mnyQty);
-        $totalPickedQty += floatval($val->mnyPickedQuantity);
-        $totalLoadedQty += floatval($val->mnyLoadedQty);
-        ?>
-        @endif
-        <?php $count++; ?>
-        @endforeach
-        <tr class="bg-dark">
-            <td colspan="3" class="text-white fw-bold">Totals</td>
-            <td class="text-white fw-bold">{{$totalWeight}}</td>
-            <td class="text-white fw-bold">{{$totalQty}}</td>
-            <td class="text-white fw-bold">{{$totalPickedQty}}</td>
-            <td class="text-white fw-bold">{{$totalLoadedQty}}</td>
-            @if(isset($includePriority) && $includePriority)
-                <td class="fw-bold">
-                    
-                </td>
-            @endif
-        </tr>
-        {{-- <tr class="bg-secondary text-white fw-bold">
-            <td colspan="7" class="text-secondary">Easter Egg</td>
-        </tr> --}}
+                <?php $count++; ?>
+                @endforeach
+                <tr class="bg-dark">
+                    <td colspan="3" class="text-white fw-bold">Totals</td>
+                    <td class="text-white fw-bold">{{$totalWeight}}</td>
+                    <td class="text-white fw-bold">{{$totalQty}}</td>
+                    <td class="text-white fw-bold">{{$totalPickedQty}}</td>
+                    <td class="text-white fw-bold">{{$totalLoadedQty}}</td>
+                    @if(isset($includePriority) && $includePriority)
+                        <td class="fw-bold">
+
+                        </td>
+                    @endif
+                </tr>
+                {{-- <tr class="bg-secondary text-white fw-bold">
+                    <td colspan="7" class="text-secondary">Easter Egg</td>
+                </tr> --}}
     </tbody>
 </table>
