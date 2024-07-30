342d5eca1d2abc03c59e1f4cd8f7f206abc4e47a
Akash Prajapati <akash.prajapati@techabbot.com>
2024-07-30 14:51:22 +0530

    feat: Do some changes related to qcphase code structure

--- dims21/resources/views/warehouse/wiredraw/qcphase/index.blade.php
+++ dims21/resources/views/warehouse/wiredraw/qcphase/index.blade.php
@@ -71,166 +71,169 @@
 
                         <div class="modal-footer mt-2">
                             <div class="d-inline-flex gap-2">
-                                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal" id="addProductcloseBtn">Close</button>
+                                <button type="button" class="btn btn-secondary close" data-bs-dismiss="modal"
+                                    id="addProductcloseBtn">Close</button>
                                 <button class="btn btn-success" id="save">Save</button>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
+        </div>
+    </div>
 
-        @endsection
+@endsection
 
-        @section('scripts')
+@section('scripts')
 
-            <script>
-                $(document).ready(function() {
-                    
-                    $('.close').click(function () {
-                        $('#saveQcData').find('.errorClass').hide()
-                        $('#general-error').hide();
-                    });
-                    
-                    $(document).on('focus', ':input', function() {
-                        $(this).attr('autocomplete', 'off');
-                    });
-                    
-                    $.ajax({
-                        url: '{{ route('wire-draw.qcscreen.get-qcscreen') }}',
-                        type: "GET",
-                        success: function(data) {
-                            const gridWorkInProgress = $("#gridQcPhase1").dxDataGrid({
-                                dataSource: data,
-                                hoverStateEnabled: true,
-                                showBorders: true,
-                                filterRow: {
-                                    visible: true
-                                },
-                                filterPanel: {
-                                    visible: true
-                                },
-                                headerFilter: {
-                                    visible: true
-                                },
-                                allowColumnResizing: true,
-                                columnAutoWidth: true,
-                                scrolling: {
-                                    rowRenderingMode: 'infinite',
-                                },
-                                paging: {
-                                    enabled: false
-                                },
-                                export: {
-                                    enabled: true
-                                },
-                                selection: {
-                                    mode: 'single',
-                                },
-                                onExporting(e) {
-                                    const workbook = new ExcelJS.Workbook();
-                                    const worksheet = workbook.addWorksheet('qc1');
+    <script>
+        $(document).ready(function() {
 
-                                    DevExpress.excelExporter.exportDataGrid({
-                                        component: e.component,
-                                        worksheet,
-                                        autoFilterEnabled: true,
-                                    }).then(() => {
-                                        workbook.xlsx.writeBuffer().then((buffer) => {
-                                            saveAs(new Blob([buffer], {
-                                                type: 'application/octet-stream'
-                                            }), 'qc1.xlsx');
-                                        });
-                                    });
-                                    e.cancel = true;
-                                },
-                                columns: [{
-                                        dataField: "intHeaderIdcustom",
-                                        caption: "Job No"
-                                    },
-                                    {
-                                        dataField: "strReference",
-                                        caption: "Reference"
-                                    },
-                                    {
-                                        dataField: "strCustomerName",
-                                        caption: "Customer Name"
-                                    },
-                                    {
-                                        dataField: "strProductName",
-                                        caption: "Product"
-                                    },
-                                    {
-                                        dataField: "dtDateStart",
-                                        caption: "Date Start"
-                                    },
-                                    {
-                                        dataField: "dtDateEnd",
-                                        caption: "Date End"
-                                    },
-                                    {
-                                        dataField: "strMachineName",
-                                        caption: "Machine"
-                                    },
-                                    {
-                                        dataField: "intNoOfStand",
-                                        caption: "Stand #"
-                                    }
-                                ],
-                                onRowDblClick: function(e) {
-                                    $('#saveQcData').modal('toggle');
-                                    var dataGrid = $("#gridQcPhase1").dxDataGrid("instance");
-                                    var selectedRowsData = dataGrid.getSelectedRowsData();
-                                    if (selectedRowsData.length > 0) {
-                                        var productName = selectedRowsData[0].strProductName;
-                                        var headerId = selectedRowsData[0].intHeaderIdcustom;
-                                        var stand = selectedRowsData[0].intNoOfStand;
+            $('.close').click(function() {
+                $('#saveQcData').find('.errorClass').hide()
+                $('#general-error').hide();
+            });
 
-                                        $('#intProductId').val(productName);
-                                        $('#intJobNumber').val(headerId);
-                                        $('#intStand').val(stand);
-                                    }
-                                },
-                                onToolbarPreparing: function(e) {
-                                    e.toolbarOptions.items.unshift({
-                                        location: 'before',
-                                        template: function() {
-                                            return $('<h3>').text('QC PHASE');
-                                        }
-                                    });
-                                },
+            $(document).on('focus', ':input', function() {
+                $(this).attr('autocomplete', 'off');
+            });
 
-                            }).dxDataGrid('instance');
-                        }
-                    });
+            $.ajax({
+                url: '{{ route('wire-draw.qcscreen.get-qcscreen') }}',
+                type: "GET",
+                success: function(data) {
+                    const gridWorkInProgress = $("#gridQcPhase1").dxDataGrid({
+                        dataSource: data,
+                        hoverStateEnabled: true,
+                        showBorders: true,
+                        filterRow: {
+                            visible: true
+                        },
+                        filterPanel: {
+                            visible: true
+                        },
+                        headerFilter: {
+                            visible: true
+                        },
+                        allowColumnResizing: true,
+                        columnAutoWidth: true,
+                        scrolling: {
+                            rowRenderingMode: 'infinite',
+                        },
+                        paging: {
+                            enabled: false
+                        },
+                        export: {
+                            enabled: true
+                        },
+                        selection: {
+                            mode: 'single',
+                        },
+                        onExporting(e) {
+                            const workbook = new ExcelJS.Workbook();
+                            const worksheet = workbook.addWorksheet('qc1');
 
-                    $('#save').click(function() {
-                        var dataGrid = $("#gridQcPhase1").dxDataGrid("instance");
-                        var selectedRowsData = dataGrid.getSelectedRowsData();
-                        console.log(selectedRowsData[0]);
-                        $.ajax({
-                            url: '{!! url('wire-draw/qcscreen') !!}',
-                            type: "POST",
-                            data: {
-                                intJobNumber: selectedRowsData[0].intHeaderId,
-                                intProductId: selectedRowsData[0].intProductId,
-                                fltWireSize: $('#fltWireSize').val(),
-                                intStand: $('#intStand').val(),
-                                strTensileTicketNumber: $('#strTensileTicketNumber').val(),
-                                strMPATolerance: $('#strMPATolerance').val(),
+                            DevExpress.excelExporter.exportDataGrid({
+                                component: e.component,
+                                worksheet,
+                                autoFilterEnabled: true,
+                            }).then(() => {
+                                workbook.xlsx.writeBuffer().then((buffer) => {
+                                    saveAs(new Blob([buffer], {
+                                        type: 'application/octet-stream'
+                                    }), 'qc1.xlsx');
+                                });
+                            });
+                            e.cancel = true;
+                        },
+                        columns: [{
+                                dataField: "intHeaderIdcustom",
+                                caption: "Job No"
                             },
-                            success: function(data) {
-                                location.reload();
+                            {
+                                dataField: "strReference",
+                                caption: "Reference"
                             },
-                            error: function(xhr) {
-                                if (xhr.status === 422) {
-                                    modalSetValidation($("#saveQcData"), xhr);
-                                } else {
-                                    console.error('An unexpected error occurred:', xhr);
-                                }
+                            {
+                                dataField: "strCustomerName",
+                                caption: "Customer Name"
+                            },
+                            {
+                                dataField: "strProductName",
+                                caption: "Product"
+                            },
+                            {
+                                dataField: "dtDateStart",
+                                caption: "Date Start"
+                            },
+                            {
+                                dataField: "dtDateEnd",
+                                caption: "Date End"
+                            },
+                            {
+                                dataField: "strMachineName",
+                                caption: "Machine"
+                            },
+                            {
+                                dataField: "intNoOfStand",
+                                caption: "Stand #"
                             }
-                        });
-                    });
-                });
-            </script>
+                        ],
+                        onRowDblClick: function(e) {
+                            $('#saveQcData').modal('toggle');
+                            var dataGrid = $("#gridQcPhase1").dxDataGrid("instance");
+                            var selectedRowsData = dataGrid.getSelectedRowsData();
+                            if (selectedRowsData.length > 0) {
+                                var productName = selectedRowsData[0].strProductName;
+                                var headerId = selectedRowsData[0].intHeaderIdcustom;
+                                var stand = selectedRowsData[0].intNoOfStand;
 
-        @endsection
+                                $('#intProductId').val(productName);
+                                $('#intJobNumber').val(headerId);
+                                $('#intStand').val(stand);
+                            }
+                        },
+                        onToolbarPreparing: function(e) {
+                            e.toolbarOptions.items.unshift({
+                                location: 'before',
+                                template: function() {
+                                    return $('<h3>').text('QC PHASE');
+                                }
+                            });
+                        },
+
+                    }).dxDataGrid('instance');
+                }
+            });
+
+            $('#save').click(function() {
+                var dataGrid = $("#gridQcPhase1").dxDataGrid("instance");
+                var selectedRowsData = dataGrid.getSelectedRowsData();
+                console.log(selectedRowsData[0]);
+                $.ajax({
+                    url: '{!! url('wire-draw/qcscreen') !!}',
+                    type: "POST",
+                    data: {
+                        intJobNumber: selectedRowsData[0].intHeaderId,
+                        intProductId: selectedRowsData[0].intProductId,
+                        fltWireSize: $('#fltWireSize').val(),
+                        intStand: $('#intStand').val(),
+                        strTensileTicketNumber: $('#strTensileTicketNumber').val(),
+                        strMPATolerance: $('#strMPATolerance').val(),
+                    },
+                    success: function(data) {
+                        location.reload();
+                    },
+                    error: function(xhr) {
+                        if (xhr.status === 422) {
+                            modalSetValidation($("#saveQcData"), xhr);
+                        } else {
+                            console.error('An unexpected error occurred:', xhr);
+                        }
+                    }
+                });
+            });
+        });
+    </script>
+
+@endsection
